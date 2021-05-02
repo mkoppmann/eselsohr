33d2d97fe4ced15ba15fe80f1a79a42449be0275
mkoppmann <dev@mkoppmann.at>
2021-05-03 00:13:08 +0200

    Put all impl specific code to repo impl module
    This makes it easier to replace the implementation with something else
    in the future.

--- eselsohr/src/Lib/Core/Effect/Repository.hs
+++ eselsohr/src/Lib/Core/Effect/Repository.hs
@@ -11,12 +11,12 @@ module Lib.Core.Effect.Repository
 
   -- * Getter helpers
   , getOne
-  , getCapIdForActId
-  , lookupCapIdForActId
+  , Impl.getCapIdForActId
+  , Impl.lookupCapIdForActId
 
   -- * 'Article' helpers
-  , artUpdateTitle
-  , artUpdateState
+  , Impl.artUpdateTitle
+  , Impl.artUpdateState
   ) where
 
 import           Lib.App                        ( App
@@ -24,7 +24,6 @@ import           Lib.App                        ( App
                                                 )
 import           Lib.Core.Domain                ( Action(..)
                                                 , Article
-                                                , ArticleState
                                                 , Capability(..)
                                                 , Context
                                                 , Entity(..)
@@ -32,10 +31,8 @@ import           Lib.Core.Domain                ( Action(..)
                                                 , Resource
                                                 , StoreEvent
                                                 )
-import qualified Lib.Core.Domain.Article       as Article
+import           Lib.Impl.Repository            ( SealedResource )
 import qualified Lib.Impl.Repository           as Impl
-
-newtype SealedResource = SealedResource {unSealResource :: Resource}
 
 data ContextState = ContextState
   { csContext  :: !Context
@@ -51,13 +48,6 @@ class (Monad m) => WriteState m where
 
 type RWState m = (ReadState m, WriteState m)
 
-instance ReadState App where
-  load = pure . SealedResource <=< Impl.load
-
-instance WriteState App where
-  init colId = Impl.init colId Impl.articleColInit
-  commit = Impl.commit
-
 class ReadResource a where
   lookup :: SealedResource -> Id a -> Maybe (Entity a)
   getMany :: SealedResource -> HashSet (Id a) -> HashMap (Id a) a
@@ -68,10 +58,17 @@ class WriteResource a where
   update :: Id a -> (a -> a) -> StoreEvent
   delete :: Id a -> StoreEvent
 
+instance ReadState App where
+  load = Impl.load
+
+instance WriteState App where
+  init   = Impl.initArtCol
+  commit = Impl.commit
+
 instance ReadResource Article where
-  lookup  = lookupArt
-  getMany = getManyArt
-  getAll  = getAllArt
+  lookup  = Impl.lookupArt
+  getMany = Impl.getManyArt
+  getAll  = Impl.getAllArt
 
 instance WriteResource Article where
   insert = Impl.insertArt
@@ -79,9 +76,9 @@ instance WriteResource Article where
   delete = Impl.deleteArt
 
 instance ReadResource Capability  where
-  lookup  = lookupCap
-  getMany = getManyCap
-  getAll  = getAllCap
+  lookup  = Impl.lookupCap
+  getMany = Impl.getManyCap
+  getAll  = Impl.getAllCap
 
 instance WriteResource Capability  where
   insert = Impl.insertCap
@@ -89,64 +86,17 @@ instance WriteResource Capability  where
   delete = Impl.deleteCap
 
 instance ReadResource Action  where
-  lookup  = lookupAct
-  getMany = getManyAct
-  getAll  = getAllAct
+  lookup  = Impl.lookupAct
+  getMany = Impl.getManyAct
+  getAll  = Impl.getAllAct
 
 instance WriteResource Action where
   insert = Impl.insertAct
   update = Impl.updateAct
   delete = Impl.deleteAct
 
+-- Helpers
+
 getOne
   :: (WithError m, ReadResource a) => SealedResource -> Id a -> m (Entity a)
 getOne sRes = Impl.asSingleEntry . lookup sRes
-
-getAllCap :: SealedResource -> HashMap (Id Capability) Capability
-getAllCap = Impl.getAll Impl.capabilityGetter . unSealResource
-
-getManyCap
-  :: SealedResource
-  -> HashSet (Id Capability)
-  -> HashMap (Id Capability) Capability
-getManyCap sRes = Impl.getMany Impl.capabilityGetter (unSealResource sRes)
-
-lookupCap :: SealedResource -> Id Capability -> Maybe (Entity Capability)
-lookupCap sRes = Impl.lookup Impl.capabilityGetter (unSealResource sRes)
-
-getAllAct :: SealedResource -> HashMap (Id Action) Action
-getAllAct = Impl.getAll Impl.actionGetter . unSealResource
-
-getManyAct
-  :: SealedResource -> HashSet (Id Action) -> HashMap (Id Action) Action
-getManyAct sRes = Impl.getMany Impl.actionGetter (unSealResource sRes)
-
-lookupAct :: SealedResource -> Id Action -> Maybe (Entity Action)
-lookupAct sRes = Impl.lookup Impl.actionGetter (unSealResource sRes)
-
-getCapIdForActId
-  :: (WithError m) => SealedResource -> Id Action -> m (Id Capability)
-getCapIdForActId sRes = Impl.asSingleEntry . lookupCapIdForActId sRes
-
-lookupCapIdForActId :: SealedResource -> Id Action -> Maybe (Id Capability)
-lookupCapIdForActId sRes = Impl.getCapIdForActId (unSealResource sRes)
-
-getAllArt :: SealedResource -> HashMap (Id Article) Article
-getAllArt = Impl.getAll Impl.articleGetter . unSealResource
-
-getManyArt
-  :: SealedResource -> HashSet (Id Article) -> HashMap (Id Article) Article
-getManyArt sRes = Impl.getMany Impl.articleGetter (unSealResource sRes)
-
-lookupArt :: SealedResource -> Id Article -> Maybe (Entity Article)
-lookupArt sRes = Impl.lookup Impl.articleGetter (unSealResource sRes)
-
--- Helper
-
-artUpdateTitle :: Id Article -> Text -> StoreEvent
-artUpdateTitle aId aTitle =
-  Impl.updateArt aId $ \oldArt -> oldArt { Article.title = aTitle }
-
-artUpdateState :: Id Article -> ArticleState -> StoreEvent
-artUpdateState aId aState =
-  Impl.updateArt aId $ \oldArt -> oldArt { Article.state = aState }
--- eselsohr/src/Lib/Impl/Repository.hs
+++ eselsohr/src/Lib/Impl/Repository.hs
@@ -1,30 +1,41 @@
 module Lib.Impl.Repository
-  ( load
+  ( SealedResource
+
+  -- * File specific functions
+  , init
+  , load
   , commit
 
-  -- * Generic implementations
-  , getAll
-  , getMany
-  , lookup
-  , init
-
-  -- * Specific implementations
-  , insertCap
-  , updateCap
-  , deleteCap
-  , insertAct
-  , updateAct
-  , deleteAct
+  -- * Domain type implementations
+  , initArtCol
+  , articleColInit
+  , articleGetter
+  , lookupArt
+  , getManyArt
+  , getAllArt
   , insertArt
   , updateArt
   , deleteArt
-  , getCapIdForActId
-  , articleGetter
-  , articleColInit
-  , capabilityGetter
+  , artUpdateTitle
+  , artUpdateState
   , actionGetter
+  , lookupAct
+  , getManyAct
+  , getAllAct
+  , insertAct
+  , updateAct
+  , deleteAct
+  , capabilityGetter
+  , lookupCap
+  , getManyCap
+  , getAllCap
+  , insertCap
+  , updateCap
+  , deleteCap
+  , getCapIdForActId
+  , lookupCapIdForActId
 
-  -- Error helper
+  -- * Helpers
   , asSingleEntry
   ) where
 
@@ -40,6 +51,7 @@ import           Lib.App                        ( AppErrorType
 import           Lib.Core.Domain                ( Action
                                                 , Article(..)
                                                 , ArticleCollection(..)
+                                                , ArticleState
                                                 , Capability(..)
                                                 , Entity(..)
                                                 , Id
@@ -48,6 +60,7 @@ import           Lib.Core.Domain                ( Action
                                                 , StoreEvent(..)
                                                 , SynchronizedStoreEvent(..)
                                                 )
+import qualified Lib.Core.Domain.Article       as Article
 import           Lib.Impl.Repository.File       ( WithFile )
 import qualified Lib.Impl.Repository.File      as File
 import           Prelude                 hiding ( getAll
@@ -55,9 +68,19 @@ import           Prelude                 hiding ( getAll
                                                 )
 import           UnliftIO.STM                   ( writeTQueue )
 
+newtype SealedResource = SealedResource {unSealResource :: Resource}
+
 type CollectionGetter a = (Resource -> HashMap (Id a) a)
 
 type CollectionSetter a = (Resource -> HashMap (Id a) a -> Resource)
+
+-- * File specific functions
+
+init :: (WithFile env m) => Id Resource -> Resource -> m ()
+init = File.init
+
+load :: (WithError m, WithFile env m) => Id Resource -> m SealedResource
+load = pure . SealedResource <=< flip File.load Prelude.id
 
 commit
   :: (WithError m, WithFile env m) => Id Resource -> Seq StoreEvent -> m ()
@@ -70,18 +93,66 @@ commit resId storeEvents = do
   -- operation, because else we get a live lock.
   atomically $ takeTMVar syncVar
 
-insertCap :: Id Capability -> Capability -> StoreEvent
-insertCap entId newEnt = SeInsertCapability
-  $ StoreData (insertSetter capabilityGetter capabilitySetter) (entId, newEnt)
+-- * Domain type implementations
 
-updateCap :: Id Capability -> (Capability -> Capability) -> StoreEvent
-updateCap entId capUpdater = SeUpdateCapability $ StoreData
-  (updateSetter capabilityGetter capabilitySetter capUpdater entId)
+initArtCol :: (WithFile env m) => Id Resource -> m ()
+initArtCol colId = init colId articleColInit
+
+articleColInit :: Resource
+articleColInit =
+  ArticleResource $ ArticleCollection Map.empty Map.empty Map.empty
+
+-- Article
+
+lookupArt :: SealedResource -> Id Article -> Maybe (Entity Article)
+lookupArt sRes = lookup articleGetter (unSealResource sRes)
+
+getManyArt
+  :: SealedResource -> HashSet (Id Article) -> HashMap (Id Article) Article
+getManyArt sRes = getMany articleGetter (unSealResource sRes)
+
+getAllArt :: SealedResource -> HashMap (Id Article) Article
+getAllArt = getAll articleGetter . unSealResource
+
+insertArt :: Id Article -> Article -> StoreEvent
+insertArt entId newEnt = SeInsertArticle
+  $ StoreData (insertSetter articleGetter articleSetter) (entId, newEnt)
+
+updateArt :: Id Article -> (Article -> Article) -> StoreEvent
+updateArt entId artUpdater = SeUpdateArticle $ StoreData
+  (updateSetter articleGetter articleSetter artUpdater entId)
   Prelude.id
 
-deleteCap :: Id Capability -> StoreEvent
-deleteCap entId = SeDeleteCapability
-  $ StoreData (deleteSetter capabilityGetter capabilitySetter) entId
+deleteArt :: Id Article -> StoreEvent
+deleteArt entId =
+  SeDeleteArticle $ StoreData (deleteSetter articleGetter articleSetter) entId
+
+articleGetter :: CollectionGetter Article
+articleGetter (ArticleResource artCol) = artCollection artCol
+
+articleSetter :: CollectionSetter Article
+articleSetter (ArticleResource artCol) newVal =
+  ArticleResource $ artCol { artCollection = newVal }
+
+artUpdateTitle :: Id Article -> Text -> StoreEvent
+artUpdateTitle aId aTitle =
+  updateArt aId $ \oldArt -> oldArt { Article.title = aTitle }
+
+artUpdateState :: Id Article -> ArticleState -> StoreEvent
+artUpdateState aId aState =
+  updateArt aId $ \oldArt -> oldArt { Article.state = aState }
+
+-- Action
+
+lookupAct :: SealedResource -> Id Action -> Maybe (Entity Action)
+lookupAct sRes = lookup actionGetter (unSealResource sRes)
+
+getManyAct
+  :: SealedResource -> HashSet (Id Action) -> HashMap (Id Action) Action
+getManyAct sRes = getMany actionGetter (unSealResource sRes)
+
+getAllAct :: SealedResource -> HashMap (Id Action) Action
+getAllAct = getAll actionGetter . unSealResource
 
 insertAct :: Id Action -> Action -> StoreEvent
 insertAct entId newEnt = SeInsertAction
@@ -96,18 +167,76 @@ deleteAct :: Id Action -> StoreEvent
 deleteAct entId =
   SeDeleteAction $ StoreData (deleteSetter actionGetter actionSetter) entId
 
-insertArt :: Id Article -> Article -> StoreEvent
-insertArt entId newEnt = SeInsertArticle
-  $ StoreData (insertSetter articleGetter articleSetter) (entId, newEnt)
+actionGetter :: CollectionGetter Action
+actionGetter (ArticleResource artCol) = artActCollection artCol
 
-updateArt :: Id Article -> (Article -> Article) -> StoreEvent
-updateArt entId artUpdater = SeUpdateArticle $ StoreData
-  (updateSetter articleGetter articleSetter artUpdater entId)
+actionSetter :: CollectionSetter Action
+actionSetter (ArticleResource artCol) newVal =
+  ArticleResource $ artCol { artActCollection = newVal }
+
+-- Capability
+
+lookupCap :: SealedResource -> Id Capability -> Maybe (Entity Capability)
+lookupCap sRes = lookup capabilityGetter (unSealResource sRes)
+
+getManyCap
+  :: SealedResource
+  -> HashSet (Id Capability)
+  -> HashMap (Id Capability) Capability
+getManyCap sRes = getMany capabilityGetter (unSealResource sRes)
+
+getAllCap :: SealedResource -> HashMap (Id Capability) Capability
+getAllCap = getAll capabilityGetter . unSealResource
+
+insertCap :: Id Capability -> Capability -> StoreEvent
+insertCap entId newEnt = SeInsertCapability
+  $ StoreData (insertSetter capabilityGetter capabilitySetter) (entId, newEnt)
+
+updateCap :: Id Capability -> (Capability -> Capability) -> StoreEvent
+updateCap entId capUpdater = SeUpdateCapability $ StoreData
+  (updateSetter capabilityGetter capabilitySetter capUpdater entId)
   Prelude.id
 
-deleteArt :: Id Article -> StoreEvent
-deleteArt entId =
-  SeDeleteArticle $ StoreData (deleteSetter articleGetter articleSetter) entId
+deleteCap :: Id Capability -> StoreEvent
+deleteCap entId = SeDeleteCapability
+  $ StoreData (deleteSetter capabilityGetter capabilitySetter) entId
+
+capabilityGetter :: CollectionGetter Capability
+capabilityGetter (ArticleResource artCol) = artCapCollection artCol
+
+capabilitySetter :: CollectionSetter Capability
+capabilitySetter (ArticleResource artCol) newVal =
+  ArticleResource $ artCol { artCapCollection = newVal }
+
+getCapIdForActId
+  :: (WithError m) => SealedResource -> Id Action -> m (Id Capability)
+getCapIdForActId sRes = asSingleEntry . lookupCapIdForActId sRes
+
+lookupCapIdForActId :: SealedResource -> Id Action -> Maybe (Id Capability)
+lookupCapIdForActId sRes actId =
+  viaNonEmpty head
+    . Map.keys
+    . Map.filter ((==) actId . actionId)
+    $ capabilityGetter (unSealResource sRes)
+
+-- * Helpers
+
+asSingleEntry :: (WithError m) => Maybe a -> m a
+asSingleEntry = withFrozenCallStack . throwOnNothing singleEntryError
+
+singleEntryError :: AppErrorType
+singleEntryError = storeError "Expected a single entry, but got none"
+
+lookup :: CollectionGetter a -> Resource -> Id a -> Maybe (Entity a)
+lookup getter res entId =
+  fmap (uncurry Entity . (entId, )) . Map.lookup entId $ getter res
+
+getMany :: CollectionGetter a -> Resource -> HashSet (Id a) -> HashMap (Id a) a
+getMany getter res entIds =
+  Map.filterWithKey (\key _ -> key `Set.member` entIds) $ getter res
+
+getAll :: CollectionGetter a -> Resource -> HashMap (Id a) a
+getAll getter = getter
 
 insertSetter
   :: CollectionGetter a
@@ -132,30 +261,6 @@ deleteSetter
   :: CollectionGetter a -> CollectionSetter a -> Resource -> Id a -> Resource
 deleteSetter getter setter = gsetter getter setter Map.delete
 
-getMany :: CollectionGetter a -> Resource -> HashSet (Id a) -> HashMap (Id a) a
-getMany getter res entIds =
-  Map.filterWithKey (\key _ -> key `Set.member` entIds) $ getter res
-
-lookup :: CollectionGetter a -> Resource -> Id a -> Maybe (Entity a)
-lookup getter res entId =
-  fmap (uncurry Entity . (entId, )) . Map.lookup entId $ getter res
-
-getAll :: CollectionGetter a -> Resource -> HashMap (Id a) a
-getAll getter = getter
-
-init :: (WithFile env m) => Id Resource -> Resource -> m ()
-init = File.init
-
-load :: (WithError m, WithFile env m) => Id Resource -> m Resource
-load = flip File.load Prelude.id
-
-getCapIdForActId :: Resource -> Id Action -> Maybe (Id Capability)
-getCapIdForActId res actId =
-  viaNonEmpty head
-    . Map.keys
-    . Map.filter ((==) actId . actionId)
-    $ capabilityGetter res
-
 gsetter
   :: CollectionGetter a
   -> CollectionSetter a
@@ -165,38 +270,3 @@ gsetter
   -> Resource
 gsetter fromCol toCol setter oldRes val =
   toCol oldRes . setter val $ fromCol oldRes
-
--- * 'Resource' specific getters and setters
-
-articleColInit :: Resource
-articleColInit =
-  ArticleResource $ ArticleCollection Map.empty Map.empty Map.empty
-
-articleGetter :: CollectionGetter Article
-articleGetter (ArticleResource artCol) = artCollection artCol
-
-articleSetter :: CollectionSetter Article
-articleSetter (ArticleResource artCol) newVal =
-  ArticleResource $ artCol { artCollection = newVal }
-
-capabilityGetter :: CollectionGetter Capability
-capabilityGetter (ArticleResource artCol) = artCapCollection artCol
-
-capabilitySetter :: CollectionSetter Capability
-capabilitySetter (ArticleResource artCol) newVal =
-  ArticleResource $ artCol { artCapCollection = newVal }
-
-actionGetter :: CollectionGetter Action
-actionGetter (ArticleResource artCol) = artActCollection artCol
-
-actionSetter :: CollectionSetter Action
-actionSetter (ArticleResource artCol) newVal =
-  ArticleResource $ artCol { artActCollection = newVal }
-
--- Error helpers
-
-asSingleEntry :: (WithError m) => Maybe a -> m a
-asSingleEntry = withFrozenCallStack . throwOnNothing singleEntryError
-
-singleEntryError :: AppErrorType
-singleEntryError = storeError "Expected a single entry, but got none"
